54fd25ff31f5b57868f36c91e090c3224b6b4e03
Jake Gadaleta <jake.gads@gmail.com>
2020-06-29 19:56:21 -0400

    setup for meeting 2

---
+++ Meeting2/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld{
+    public static void main(String[] args){
+        String name = "Joe";
+        System.out.println("Hi " + name);
+        name = "Jake";
+        System.out.println("Hiya " + name);
+    }
+}
