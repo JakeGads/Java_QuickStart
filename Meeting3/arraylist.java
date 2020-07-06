1294747bc545d38616a483a8389f1ccbbc10b61a
Jake Gadaleta <jake.gads@gmail.com>
2020-07-06 19:40:11 -0400

    Syncing up to the repo

--- Meeting3/arraylist.java
+++
@@ -1,8 +0,0 @@
-import java.util.ArrayList;
-
-public class arraylist{
-    public static void main(String[] args){
-        ArrayList<Integer> myArrayList = new ArrayList<Integer>();
-        myArrayList.add(5);
-    }
-}
